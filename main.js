0a43b058fb1f407c99c963b9e41403f4495907b1
Alex Bertotto <ajb5206@gmail.com>
2024-01-23 16:11:20 -0800

    add new prompt

--- main.js
+++ main.js
@@ -72,3 +72,10 @@ function isDivisible(...nums){
 	}
 	return true;
 }
+
+// You are given a list/array which contains only integers (positive and negative). Your job is to sum only the numbers that are the same and consecutive. The result should be one list.
+
+// Extra credit if you solve it in one line. You can assume there is never an empty list/array and there will always be an integer.
+
+// Same meaning: 1 == 1
+
