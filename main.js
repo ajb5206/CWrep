17f3f79fdb45c5415642c4f591625241f0f9bd63
Alex Bertotto <ajb5206@gmail.com>
2024-01-25 10:40:48 -0800

    add prompt and starting function

--- main.js
+++ main.js
@@ -164,3 +164,11 @@ function partsSums(ls){
 	return finalLs;
 }
 //currently not performant 
+
+// In this kata, you will write a function that receives an array of string and returns a string that is either 'naughty' or 'nice'. Strings that start with the letters b, f, or k are naughty. Strings that start with the letters g, s, or n are nice. Other strings are neither naughty nor nice.
+
+// If there is an equal amount of bad and good actions, return 'naughty'
+
+function whatListAmIOn(actions){
+	
+}
