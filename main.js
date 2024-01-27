080172bcd1a5293e5199b5464167db48e4aa11ee
Alex Bertotto <ajb5206@gmail.com>
2024-01-27 11:03:19 -0800

    add new prompt and starting function

--- main.js
+++ main.js
@@ -206,3 +206,23 @@ function doMath(string){
 
 	//implement rolling + - * / 
 }
+
+//https://www.codewars.com/kata/5a63948acadebff56f000018/train/javascript
+// Task
+// Given an array/list [] of integers , Find the product of the k maximal numbers.
+
+// Notes
+// Array/list size is at least 3 .
+
+// Array/list's numbers Will be mixture of positives , negatives and zeros
+
+// Repetition of numbers in the array/list could occur.
+
+// Input >> Output Examples
+// maxProduct ({4, 3, 5}, 2) ==>  return (20)
+// Explanation:
+// Since the size (k) equal 2 , then the subsequence of size 2 whose gives product of maxima is 5 * 4 = 20 .
+
+function maxProduct(numbers, size){
+	
+}
