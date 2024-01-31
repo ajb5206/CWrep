305f4b1d59a7d66f33145bc73b5d2d0c9ba8db4b
Alex Bertotto <ajb5206@gmail.com>
2024-01-30 18:29:30 -0800

    add prompt and starting function

--- main.js
+++ main.js
@@ -265,3 +265,15 @@ function squareUp(n)
 // 0021
 // 0321
 // 4321
+
+// Prime Factors
+// Inspired by one of Uncle Bob's TDD Kata
+
+// Write a function that generates factors for a given number.
+
+// The function takes an integer as parameter and returns a list of integers (ObjC: array of NSNumbers representing integers). That list contains the prime factors in numerical sequence.
+
+function primeFactors(n) {
+	
+	return [];
+}
